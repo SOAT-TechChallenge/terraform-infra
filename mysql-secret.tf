35a9cf025ee331fc230d39c659970f9e7e12ef56
leynerbueno <leyner_henrique@hotmail.com.br>
2025-09-20 13:20:13 -0300

    Update mysql-secret.tf

--- mysql-secret.tf
+++ mysql-secret.tf
@@ -6,7 +6,7 @@ resource "kubernetes_secret" "mysql_secret" {
   data = {
     MYSQL_ROOT_PASSWORD        = "root12345" 
     SPRING_DATASOURCE_PASSWORD = "root12345" 
-    SPRING_DATASOURCE_URL      = "jdbc:mysql://tech-challenge-db.cbia2wmkcvfh.us-east-1.rds.amazonaws.com:3306/fiap"
+    SPRING_DATASOURCE_URL      = "jdbc:mysql://${aws_db_instance.mysql_database.endpoint}/fiap"
     SPRING_DATASOURCE_USERNAME = "admin"     
     EMAIL_USER                 = "techchallenge.noreply@gmail.com"  
     EMAIL_PASS                 = "sbjmrdfduwjdaqhn"  
