c98d5581b6ad455153e255ce0ba8e4bb101a0e3a
lucianaTSoares <lucianat.s@hotmail.com>
2025-09-30 22:22:20 -0300

    Refactor: remove old resources and implement new EKS setup with VPC, deployment, service, ingress, and autoscaler configurations

---
+++ app-k8s.tf
@@ -0,0 +1,233 @@
+resource "kubernetes_deployment" "app_deployment" {
+  metadata {
+    name = "techchallenge"
+    labels = {
+      app = "techchallenge"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "techchallenge"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "techchallenge"
+        }
+      }
+
+      spec {
+        container {
+          name  = "techchallenge"
+          image = "leynerbueno/tech-challenge:latest"
+
+          port {
+            container_port = 8080
+          }
+
+          env {
+            name = "SPRING_DATASOURCE_URL"
+            value_from {
+              secret_key_ref {
+                name = "mysql-secret"
+                key  = "SPRING_DATASOURCE_URL"
+              }
+            }
+          }
+
+          env {
+            name = "SPRING_DATASOURCE_USERNAME"
+            value_from {
+              secret_key_ref {
+                name = "mysql-secret"
+                key  = "SPRING_DATASOURCE_USERNAME"
+              }
+            }
+          }
+
+          env {
+            name = "SPRING_DATASOURCE_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = "mysql-secret"
+                key  = "SPRING_DATASOURCE_PASSWORD"
+              }
+            }
+          }
+
+          env {
+            name = "EMAIL_USER"
+            value_from {
+              secret_key_ref {
+                name = "mysql-secret"
+                key  = "EMAIL_USER"
+              }
+            }
+          }
+
+          env {
+            name = "EMAIL_PASS"
+            value_from {
+              secret_key_ref {
+                name = "mysql-secret"
+                key  = "EMAIL_PASS"
+              }
+            }
+          }
+
+          resources {
+            limits = {
+              cpu    = "1"
+              memory = "1Gi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "512Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/actuator/health/liveness"
+              port = 8080
+            }
+            initial_delay_seconds = 60
+            period_seconds        = 10
+            failure_threshold     = 3
+            timeout_seconds       = 5
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/actuator/health/readiness"
+              port = 8080
+            }
+            initial_delay_seconds = 30
+            period_seconds        = 5
+            failure_threshold     = 3
+            timeout_seconds       = 5
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_secret.mysql_secret,
+    helm_release.ingress_nginx
+  ]
+}
+
+resource "kubernetes_service" "app_service" {
+  metadata {
+    name = "techchallenge-service"
+  }
+
+  spec {
+    selector = {
+      app = "techchallenge"
+    }
+
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 8080
+      protocol    = "TCP"
+    }
+
+    type = "ClusterIP"
+  }
+
+  depends_on = [kubernetes_deployment.app_deployment]
+}
+
+resource "kubernetes_ingress_v1" "app_ingress" {
+  metadata {
+    name = "techchallenge-ingress"
+    annotations = {
+      "kubernetes.io/ingress.class"                        = "nginx"
+      "nginx.ingress.kubernetes.io/enable-cors"            = "true"
+      "nginx.ingress.kubernetes.io/cors-allow-methods"     = "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+      "nginx.ingress.kubernetes.io/cors-allow-origin"      = "*"
+      "nginx.ingress.kubernetes.io/cors-allow-credentials" = "true"
+      "nginx.ingress.kubernetes.io/cors-allow-headers"     = "Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization"
+      "nginx.ingress.kubernetes.io/cors-max-age"           = "86400"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    rule {
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.app_service.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "hpa" {
+  metadata {
+    name = "techchallenge-hpa"
+  }
+
+  spec {
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = "techchallenge"
+    }
+
+    min_replicas                      = 1
+    max_replicas                      = 10
+    target_cpu_utilization_percentage = 50
+  }
+
+  depends_on = [kubernetes_deployment.app_deployment]
+}
+
+resource "kubernetes_secret" "mysql_secret" {
+  metadata {
+    name = "mysql-secret"
+  }
+
+  data = {
+    MYSQL_ROOT_PASSWORD        = "root12345"
+    SPRING_DATASOURCE_PASSWORD = "root12345"
+    SPRING_DATASOURCE_URL      = "jdbc:mysql://tech-challenge-db.cwhwodgptwy8.us-east-1.rds.amazonaws.com:3306/fiap"
+    SPRING_DATASOURCE_USERNAME = "admin"
+    EMAIL_USER                 = "techchallenge.noreply@gmail.com"
+    EMAIL_PASS                 = "sbjmrdfduwjdaqhn"
+  }
+}
+
+output "app_deployment_name" {
+  description = "Deployment name"
+  value       = kubernetes_deployment.app_deployment.metadata[0].name
+}
+
+output "app_service_name" {
+  description = "Service name"
+  value       = kubernetes_service.app_service.metadata[0].name
+}
+
+output "ingress_name" {
+  description = "Ingress name"
+  value       = kubernetes_ingress_v1.app_ingress.metadata[0].name
+}
---
+++ nlb.tf
@@ -0,0 +1,32 @@
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "kube-system"
+  version    = "4.13.2"
+
+  recreate_pods = true
+
+  set = [
+    {
+      name  = "controller.service.enabled"
+      value = "true"
+    },
+    {
+      name  = "controller.service.type"
+      value = "LoadBalancer"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-internal"
+      value = "true"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-subnets"
+      value = "${join("\\,", [data.aws_subnet.eks_subnet_private_1.id, data.aws_subnet.eks_subnet_private_2.id])}"
+    }
+  ]
+}
